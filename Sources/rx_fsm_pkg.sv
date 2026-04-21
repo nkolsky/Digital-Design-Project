5563ac644037e4175433e103d37cc29ebe332197
Naomi Kolsky <kolskynaomi@gmail.com>
2026-04-21 20:35:27 +0300

    updated the names

--- Sources/rx_fsm_pkg.sv
+++ Sources/rx_fsm_pkg.sv
@@ -4,7 +4,7 @@ package rx_fsm_pkg;
         VALIDATE_START =    3'b001, 
         READ_TO_REG =       3'b010, 
         VALIDATE_STOP =     3'b011, 
-        UPDATE_BYTE_COUNT = 3'b100, 
-        DELAY =             3'b101, 
+        UPDATE_BYTE_CNT = 3'b100, 
+        INTER_BIT_DELAY =             3'b101, 
         PARSE_DATA =        3'b110} rx_state_t;
 endpackage : rx_fsm_pkg
