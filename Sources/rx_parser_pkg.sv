edca7c9b71e99063e7c390325203e7125652418f
Naomi Kolsky <kolskynaomi@gmail.com>
2026-04-23 14:20:08 +0300

    added comma to the package

--- Sources/rx_parser_pkg.sv
+++ Sources/rx_parser_pkg.sv
@@ -6,5 +6,6 @@ localparam [7:0] CHAR_R = 8'h52; // 'R'
 localparam [7:0] CHAR_C = 8'h43; // 'C'
 localparam [7:0] CHAR_V = 8'h56; // 'V'
 localparam [7:0] ASCII_ZERO = 8'h30; // '0'
+localparam [7:0] CHAR_COMMA = 8'h2C; // ','
 
 endpackage : rx_parser_pkg
