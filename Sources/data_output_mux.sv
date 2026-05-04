caf52ca44de9790c035f2892db58ccc8c2bd33e2
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 18:06:26 +0300

    add tx package for local params

--- Sources/data_output_mux.sv
+++ Sources/data_output_mux.sv
@@ -1,11 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-
-// Design Name: TX Controller
-// Module Name: data_output_mux
-// Project Name: Lab4
-//////////////////////////////////////////////////////////////////////////////////
-
+import tx_fsm_pkg::*;
 
 module data_output_mux(
     input logic [7:0] data_in,
@@ -16,9 +10,9 @@ module data_output_mux(
 always_comb begin //: byte_selector
     case (select)
         2'b00: mux_out = data_in; // default to data input
-        2'b01: mux_out = 8'h20; // Space (' ')
-        2'b10: mux_out = 8'h0A; // Line Feed (\n)
-        2'b11: mux_out = 8'h0D; // Carriage return (\r)
+        2'b01: mux_out = CHAR_SPACE; // Space (' ')
+        2'b10: mux_out = CHAR_LF; // Line Feed (\n)
+        2'b11: mux_out = CHAR_CR; // Carriage return (\r)
         //default: mux_out = data_in; //default to data input
     endcase
 end //: byte_selector
