5c42d5f0ccd2a322c552e1f93b8152fe1dfa7fee
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 18:06:47 +0300

    added local params

--- Sources/tx_fsm_pkg.sv
+++ Sources/tx_fsm_pkg.sv
@@ -10,5 +10,10 @@ package tx_fsm_pkg;
         SEND_SPACE =    4'b0111, 
         WAIT_SPACE =    4'b1000
         } tx_state_t;
+
+    localparam logic [7:0] CHAR_SPACE = 8'h20;
+    localparam logic [7:0] CHAR_CR    = 8'h0D;
+    localparam logic [7:0] CHAR_LF    = 8'h0A;
+    
 endpackage : tx_fsm_pkg
 
