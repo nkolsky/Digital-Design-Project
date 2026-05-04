c037c7fb2fb5e983b263ba72b1cdeeb9952fef55
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 22:12:15 +0300

    undid a balagan with the state names and declared properly

--- Sources/tx_fsm_pkg.sv
+++ Sources/tx_fsm_pkg.sv
@@ -1,14 +1,15 @@
 package tx_fsm_pkg;
     typedef enum logic [3:0] {
-        IDLE =          4'b0000, 
-        WAIT_DATA =     4'b0001, 
-        SEND_NEW_LINE = 4'b0010, 
-        WAIT_NEW_LINE = 4'b0011, 
-        SEND_LINE_START = 4'b0100, 
-        WAIT_LINE_START = 4'b0101, 
-        WAIT_NEW_BYTE = 4'b0110, 
-        SEND_SPACE =    4'b0111, 
-        WAIT_SPACE =    4'b1000
+        IDLE = 4'd1, 
+        SEND_DATA = 4'd2, 
+        WAIT_DATA = 4'd3,
+        SEND_NEW_LINE = 4'd4, 
+        WAIT_NEW_LINE = 4'd5, 
+        SEND_LINE_START = 4'd6,
+        WAIT_LINE_START = 4'd7, 
+        WAIT_NEW_BYTE = 4'd8, 
+        SEND_SPACE = 4'd9,
+        WAIT_SPACE = 4'd10
         } tx_state_t;
 
     localparam logic [7:0] CHAR_SPACE = 8'h20;
