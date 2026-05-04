ad9fd3397ecceb9d1276df24115b17343c33d787
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 14:48:07 +0300

    changed some comment formatting and added the run tick counter and clear tick counter logic to the tick counter

--- Sources/rx_fsm.sv
+++ Sources/rx_fsm.sv
@@ -8,35 +8,37 @@ module rx_fsm (
     input logic [2:0] bit_cnt, //bit count for current byte being received
     input logic [3:0] byte_cnt, //how many bytes read out of the 16 bytes in the message
     input logic rx_mode, //default to IDLE when low (in TX mode on low, RX mode on high)
-    
+
     //removing the line below because interbit delay is for tx, not rx
     //input logic timer_done, //indicates when the inter-bit delay timer has completed counting
 
     //moore outputs
-    //output logic clr_tick_cntr,
-    //output logic run_tick_cntr,
     output logic shift_en, 
     output logic bit_cnt_en,
     output logic byte_cnt_en,
     output logic msg_reg_en,
     output logic parse_en
-);    
+);
 
 //Define internal signals
 rx_state_t cur_state, next_state;
 
 //Create tick_q counter for validating start and stop bits
-logic [3:0] tick_q; //need to count up to 16 ticks (0-15) for validating start and stop bits and knowing when to shift in bits
-
+/**need to count up to 16 ticks (0-15) for validating start and stop bits and knowing when to shift in bits**/
+logic clr_tick_cntr;
+logic [3:0] tick_q;
+logic run_tick_cntr;
 always_ff @(posedge clk or negedge rst_n) begin : tickCounter
     if (!rst_n) begin
         tick_q <= 4'b0;
-    end else if (tick) begin
+    end else if (clr_tick_cntr) begin
+        tick_q <= 4'b0;
+    end else if (run_tick_cntr && tick) begin
         tick_q <= tick_q + 1;
     end else begin
         tick_q <= tick_q; //hold value when tick_q is low
     end
-    
+
 end : tickCounter
 
 
@@ -59,7 +61,7 @@ always_ff @(posedge clk) begin : validateStartShiftReg
     end else begin
         start_window <= 3'b111; //default to all 1s when not validating start bit
     end
-    
+
 end : validateStartShiftReg
 
 // Shift reg for validating stop bit
@@ -73,7 +75,7 @@ always_ff @(posedge clk) begin : validateStopShiftReg
     end else begin
         stop_window <= 3'b000; //default to all 0s when not validating stop bit
     end
-    
+
 end : validateStopShiftReg
 
 // Next State Logic (Combinational)
@@ -93,9 +95,10 @@ always_comb begin : rx_nextStateLogic
                 next_state = READ_TO_REG;
         end
         READ_TO_REG: begin
-            if(bit_cnt == 3'd7 && tick_q == 4'd15) //if we've read in all 8 bits of the byte and the tick_q counter is at the center of the bit period
+            //if we've read in all 8 bits of the byte and the tick_q counter is at the center of the bit period
+            if(bit_cnt == 3'd7 && tick_q == 4'd15)
                 next_state = VALIDATE_STOP;
-            
+
         end
         VALIDATE_STOP: begin
             if(tick_q == 4'd10) //we passed the middle of the stop bit, time to validate our shift reg
@@ -108,7 +111,8 @@ always_comb begin : rx_nextStateLogic
             if(byte_cnt == 4'd15) //if we've received all 16 bytes in the message
                 next_state = PARSE_DATA;
             else
-                next_state = IDLE; //if we haven't received all 16 bytes, we go back to idle and wait for the next start bit.
+                //if we haven't received all 16 bytes, we go back to idle and wait for the next start bit.
+                next_state = IDLE;
                 //removing the interbit delay
                 //next_state = INTER_BIT_DELAY; //otherwise we need to wait the inter-bit delay before looking for the next start bit
         end
@@ -118,18 +122,19 @@ always_comb begin : rx_nextStateLogic
                     next_state = VALIDATE_START;
                 else //otherwise we go back to idle and wait for the next start bit
                     next_state = IDLE;
-            else 
-                next_state = INTER_BIT_DELAY; //otherwise we stay in the inter-bit delay state until the timer is done    
+            else
+                next_state = INTER_BIT_DELAY; //otherwise we stay in the inter-bit delay state until the timer is done
         end*/
         PARSE_DATA: begin
             //check if its enough to pulse en_parse for one cycle here, or if we need to stay in this state until parsing is done
-            next_state = IDLE; //after parsing the data, we go back to idle and wait for the next message
+            //after parsing the data, we go back to idle and wait for the next message
+            next_state = IDLE;
         end
         default: begin
             next_state = IDLE;
         end
     endcase
-    
+
 end : rx_nextStateLogic
 
 //Moore Output Logic (Sequential)
@@ -151,8 +156,8 @@ always_ff @(posedge clk or negedge rst_n) begin : rx_outputLogic
         byte_cnt_en   <= 1'b0;
         msg_reg_en    <= 1'b0;
         parse_en      <= 1'b0;
-    
-        case(cur_state)
+
+        unique case(cur_state)
             IDLE: begin
                 clr_tick_cntr <= 1'b1; // Keep tick_q at 0 until start detected
             end
@@ -161,8 +166,8 @@ always_ff @(posedge clk or negedge rst_n) begin : rx_outputLogic
                 //see start_window logic above for how we use the shift reg to validate the start bit at the right time
             end
             READ_TO_REG: begin
-                run_tick_cntr <= 1'b1; // Keep counting ticks to know when we're in the middle of the bit period
-
+                // Keep counting ticks to know when we're in the middle of the bit period
+                run_tick_cntr <= 1'b1;
                 // Sample in the middle of the bit period (tick_q == 8) to shift in the bit to the temporary byte shift register
                 if (tick_q == 4'd8) begin
                     shift_en <= 1'b1;
@@ -173,22 +178,27 @@ always_ff @(posedge clk or negedge rst_n) begin : rx_outputLogic
                 end
             end
             VALIDATE_STOP: begin
-                run_tick_cntr <= 1'b1; // Start counting ticks to validate stop bit at the right time
+                // Start counting ticks to validate stop bit at the right time
+                run_tick_cntr <= 1'b1;
                 //see stop_window logic above for how we use the shift reg to validate the stop bit
             end
             UPDATE_BYTE_CNT: begin
                 byte_cnt_en <= 1'b1;
-                msg_reg_en <= 1'b1; //latch the byte we just received into the correct position in the message register
-                clr_tick_cntr <= 1'b1; //reset tick_q counter to prepare for validating the next start bit after the inter-bit delay
+                //latch the byte we just received into the correct position in the message register
+                msg_reg_en <= 1'b1;
+                //reset tick_q counter to prepare for validating the next start bit after the inter-bit delay
+                clr_tick_cntr <= 1'b1;
             end
             INTER_BIT_DELAY: begin
-                clr_tick_cntr <= 1'b1; //keep tick_q counter at 0 while waiting for the inter-bit delay timer to finish
-            end      
+                //keep tick_q counter at 0 while waiting for the inter-bit delay timer to finish
+                clr_tick_cntr <= 1'b1;
+            end
             PARSE_DATA: begin
-                parse_en <= 1'b1; //enable the parsing of the message after we've received all 16 bytes
+                //enable the parsing of the message after we've received all 16 bytes
+                parse_en <= 1'b1;
             end
         endcase
-    end        
+    end
 end : rx_outputLogic
 
 
