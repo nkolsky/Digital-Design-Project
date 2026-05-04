4a0d8af76de51df23230704b8e6fd6f2132cf7f3
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 22:11:39 +0300

    fixed i/os

--- Sources/svn_seg_data_slct.sv
+++ Sources/svn_seg_data_slct.sv
@@ -6,9 +6,9 @@ module svn_seg_data_slct(
     input [7:0] size_val,
     input [7:0] speed_val,
     input [2:0] bit_cnt,
-    input logic [7:0] pixel_val,
-    input logic [7:0] col_val,
-    input logic [7:0] row_val,
+    input logic [7:0] rx_pixel,
+    input logic [7:0] rx_col,
+    input logic [7:0] rx_row,
     input logic rx_mode, // RX mode when high, TX mode when low
     output logic decimal,
     output logic [3:0] disp_val
@@ -19,11 +19,11 @@ always_comb begin
     if (rx_mode)begin
         unique case (bit_cnt) 
             3'b000: begin
-            disp_val = pixel_val[3:0];
+            disp_val = rx_pixel[3:0];
             decimal = 1'b1;
             end
             3'b001: begin
-            disp_val = pixel_val[7:4];
+            disp_val = rx_pixel[7:4];
             decimal = 1'b1;
             end
             3'b010: begin
@@ -35,19 +35,19 @@ always_comb begin
             decimal = 1'b1;
             end
             3'b100: begin
-            disp_val = col_val[3:0];
+            disp_val = rx_col[3:0];
             decimal = 1'b1;
             end
             3'b101: begin
-            disp_val = col_val[7:4];
+            disp_val = rx_col[7:4];
             decimal = 1'b1;
             end
             3'b110: begin
-            disp_val = row_val[3:0];
+            disp_val = rx_row[3:0];
             decimal = 1'b1;
             end
             3'b111: begin
-            disp_val = row_val[7:4];
+            disp_val = rx_row[7:4];
             decimal = 1'b0;
             end
         endcase
