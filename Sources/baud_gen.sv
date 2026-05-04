81b9b708e779e6436b70c1e06cb5ca53cc31e05c
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 14:07:11 +0300

    added counter logic

--- Sources/baud_gen.sv
+++ Sources/baud_gen.sv
@@ -16,12 +16,26 @@ localparam  DIV_TX= 1736; //100 MHz / 57.6 kHz = 1736.1, round down
 //calculated sys clk / (baud rate * 16)
 localparam DIV_RX = 109; //100 MHz / (57.6 kHz * 16) = 108.5, round up
 
-
+//we will use the same baud gen for both tx and rx, just with different divisors, so we can save area by only instantiating one
+logic [10:0] counter; //11 bits to count up to 1736 (but can actually reach 2047, but we will reset at 1736 or 109 depending on mode)
 logic [10:0]divisor;
 
 assign divisor = rx_mode ? DIV_RX : DIV_TX; //select divisor based on mode, we can use the same baud gen for both tx and rx by just changing the divisor
 
-//registers for counting
+//counter logic
+always_ff @(posedge clk or negedge rst_n) begin : baudCounter
+    if (!rst_n) begin
+        counter <= 11'b0;
+        tick_16x <= 1'b0;
+    end else if (counter == divisor - 1) begin //when we reach the divisor, we need to reset the counter and generate a tick
+        counter <= 11'b0;
+        tick_16x <= 1'b1; //generate a tick
+    end else begin
+        counter <= counter + 1; //increment counter
+        tick_16x <= 1'b0; //keep tick low until we reach the divisor
+    end
+    
+end : baudCounter
 
 
 endmodule : baud_gen
