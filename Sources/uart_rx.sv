3ccc748cd3633453cf05e65be652de6b2af2bffc
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 16:04:35 +0300

    added input and output ports for uart_rx

--- Sources/uart_rx.sv
+++ Sources/uart_rx.sv
@@ -1,9 +1,18 @@
 import rx_fsm_pkg::*;
 
 module uart_rx (
-   
+    input logic clk,
+    input logic rst_n,
+    input logic rx_in,
+    input logic rx_mode, // TX mode when low, RX mode when high
+
+    output logic [7:0] row_out,
+    output logic [7:0] col_out,
+    output logic [7:0] pix_out
 );
 
-);
+//internal signals
+
+
 
 endmodule
