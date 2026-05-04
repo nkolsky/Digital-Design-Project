be5740611e3cd89af8d77ef312a4f97a5ced34f7
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-04 18:41:41 +0300

    change back to 4  bits for dash in rx mode

--- Sources/svn_seg_decoder.sv
+++ Sources/svn_seg_decoder.sv
@@ -21,7 +21,7 @@
 
 
 module svn_seg_decoder( //break this out into a data selection mux (new module)
-    input logic [4:0] disp_val, //holds a value to display
+    input logic [3:0] disp_val, //holds a value to display
     input logic dec_in,
     //input logic [2:0] bit_cnt,
     output logic [6:0] seg_out,
@@ -34,25 +34,24 @@ always @(*) begin
     dec_out <= dec_in;
     
     case(disp_val)
-        5'b00000: seg_out <= 7'b0000001; // 0
-        5'b00001: seg_out <= 7'b1001111; // 1
-        5'b00010: seg_out <= 7'b0010010; // 2
-        5'b00011: seg_out <= 7'b0000110; // 3
-        5'b00100: seg_out <= 7'b1001100; // 4
-        5'b00101: seg_out <= 7'b0100100; // 5
-        5'b00110: seg_out <= 7'b0100000; // 6
-        5'b00111: seg_out <= 7'b0001111; // 7
-        5'b01000: seg_out <= 7'b0000000; // 8
-        5'b01001: seg_out <= 7'b0000100; // 9
-        5'b01010: seg_out <= 7'b0001000; // A
-        5'b01011: seg_out <= 7'b1100000; // B
-        5'b01100: seg_out <= 7'b0110001; // C
-        5'b01101: seg_out <= 7'b0100001;  // D
-        5'b01110: seg_out <= 7'b0110000; // E
-        5'b01111: seg_out <= 7'b0111000; // F
-        5'b10000: seg_out <= 7'b1111110; //  '-' for rx mode t1
-        default: seg_out <= 7'b1111111; // default to off
-
+        4'b0000: seg_out <= 7'b0000001; // 0
+        4'b0001: seg_out <= 7'b1001111; // 1
+        4'b0010: seg_out <= 7'b0010010; // 2
+        4'b0011: seg_out <= 7'b0000110; // 3
+        4'b0100: seg_out <= 7'b1001100; // 4
+        4'b0101: seg_out <= 7'b0100100; // 5
+        4'b0110: seg_out <= 7'b0100000; // 6
+        4'b0111: seg_out <= 7'b0001111; // 7
+        4'b1000: seg_out <= 7'b0000000; // 8
+        4'b1001: seg_out <= 7'b0000100; // 9
+        4'b1010: seg_out <= 7'b0001000; // A
+        4'b1011: seg_out <= 7'b1100000; // b
+        4'b1100: seg_out <= 7'b0110001; // C
+        4'b1101: seg_out <= 7'b1000010; // d
+        4'b1110: seg_out <= 7'b0110000; // E
+        4'b1111: seg_out <= 7'b0111000; // F
+        
+        default: seg_out <= 7'b1111111; //display off.  
     endcase
         
 end
