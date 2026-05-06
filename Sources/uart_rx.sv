fcfdf15425b2021e8c33054caee56bc52b4f23cb
Naomi Kolsky <kolskynaomi@gmail.com>
2026-05-06 10:38:20 +0300

    ..

--- Sources/uart_rx.sv
+++ Sources/uart_rx.sv
@@ -3,7 +3,7 @@ import rx_fsm_pkg::*;
 module uart_rx (
     input logic clk,
     input logic rst_n,
-    input logic rx_in,
+    input logic rx_in, 
     input logic rx_mode, // TX mode when low, RX mode when high
 
     output logic [7:0] row_out,
